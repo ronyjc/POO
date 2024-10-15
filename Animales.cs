9a5be6962ef529d9e9dcdcbcadebec4a1ce2eeb1
ronyjc <ronyjcifuentes@gmail.com>
2024-10-15 15:23:56 -0600

    primera carga

---
+++ Animales.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOEjemplo
+{
+    public class Animales
+    {
+        string especie;
+        int edad;
+        string clasificacion;
+
+        public Animales(string esp, int ed, string cla) 
+        {
+            especie = esp;
+            edad = ed;
+            clasificacion = cla;
+        
+        }
+
+        public string getEspecie() { 
+            
+            return especie;
+        
+        }
+
+        public int getEdad() { 
+        
+            return edad;
+        }
+
+        public string getClasificacion() { 
+        
+            return clasificacion;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using POOEjemplo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido, vamos a crear un objeto de la clase animales");
+        Console.WriteLine("Por favor ingrese la especie");
+        string especie = Console.ReadLine();
+        Console.WriteLine("Por favor ingrese la edad");
+        int edad = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Por favor ingrese la clasificación");
+        string clasificacion = Console.ReadLine();
+
+        Animales animal1 = new Animales(especie, edad, clasificacion);
+        Console.WriteLine("El animal ingresado es: ");
+        Console.WriteLine(animal1.getEspecie());
+        Console.WriteLine(animal1.getEdad());
+        Console.WriteLine(animal1.getClasificacion());
+    }
+}
